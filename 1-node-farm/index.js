260cfa43a93ff200cd3cf5bdcee1ab0793be1651
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-24 20:31:47 +0530

    ⚡️ Error in callback function

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -12,6 +12,7 @@ console.log("File written");
 
 //Non-Blocking Asynchronous Way
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log("ERROR!"); 
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
