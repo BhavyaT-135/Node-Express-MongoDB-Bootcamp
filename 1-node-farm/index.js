df3dcd077ded1ea1fcde19884c3fecada2e4c340
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-31 04:15:42 +0530

    🐛 Minor bug fixes in api creation

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -34,7 +34,7 @@ const url = require('url');
 //////////////////////////////////////////
 /////SERVER
 
-const data = fs.readFileSync('./txt/start.txt', 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
