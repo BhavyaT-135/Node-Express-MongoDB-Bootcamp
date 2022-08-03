097843e252f267eff72ef38f3a0794a2d07ed8ea
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-08-03 17:23:14 +0530

    ⚡️Setting route and displaying product template

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -56,11 +56,10 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const {query, pathname} =  url.parse(req.url, true);
-    const pathName = req.url;
+    const { query, pathname } = url.parse(req.url, true);
 
     //Overview Page
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html' });
         
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join(''); 
@@ -70,13 +69,15 @@ const server = http.createServer((req, res) => {
     }
 
     //Product Page
-    else if (pathName === '/product') {
-        console.log(query);
-        res.end("This is the PRODUCT!");
+    else if (pathname === '/product') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
     }
         
     //API
-    else if (pathName === '/api') {
+    else if (pathname === '/api') {
         res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(data);
     }
