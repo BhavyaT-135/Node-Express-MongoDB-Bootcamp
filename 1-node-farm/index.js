577cf0f456ed25b6bdfd059631f430c45c762db9
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-08-04 16:08:22 +0530

    ⚡️ Using slugify - 3rd party module

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,7 +1,8 @@
-//Importing the fs module
+//Importing the fs, http, url module
 const fs = require("fs");
 const http = require('http');
 const url = require('url'); 
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 //////////////////////////////////////////
@@ -41,6 +42,11 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
+console.log(slugify('Fresh Avocados', { lower: true }));
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
