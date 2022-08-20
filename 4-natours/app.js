a587d7d353b0fe6a2af7ce205e9d0daba214d291
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-08-20 16:51:51 +0530

    ⚡️ Handling DELETE Requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -86,6 +86,24 @@ app.post('/api/v1/tours', (req, res) => {
 //     });
 // })
 
+// app.delete('/api/v1/tours/:id', (req, res) => {
+//     const id = req.params.id * 1;
+//     const tour = tours.find(el => el.id === id);
+
+//     if (!tour) {
+//         return res.status(404).json({
+//             status: 'fail',
+//             message: 'Invalid ID',
+//         });
+//     }
+
+//     tours = tours.filter(el => el.id !== id);
+//     res.status(204).json({
+//         status: 'success',
+//         data: null,
+//     });
+// })
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
