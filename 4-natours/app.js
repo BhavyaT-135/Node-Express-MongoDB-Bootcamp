0768504a22dcd04e3e66415f6c31fc8353b963c6
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-08-20 16:50:29 +0530

    ⚡️ Handling PATCH Requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -66,6 +66,26 @@ app.post('/api/v1/tours', (req, res) => {
     });
 });
 
+// app.patch('/api/v1/tours/:id', (req, res) => {
+//     const id = req.params.id * 1;
+//     const tour = tours.find(el => el.id === id);
+
+//     if (!tour) {
+//         return res.status(404).json({
+//             status: 'fail',
+//             message: 'Invalid ID',
+//         });
+//     }
+
+//     const updatedTour = Object.assign(tour, req.body);
+//     res.status(200).json({
+//         status: 'success',
+//         data: {
+//             tour: updatedTour,
+//         }
+//     });
+// })
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
